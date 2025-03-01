902b255ba0081098d68f537b3b0d2bd55bd716d6
datasense <datasense.learning@gmail.com>
2025-03-01 09:42:51 -0700

    Second Change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")  # Ensure the root path ("/") is defined
 def home():
-    return {"message": "Welcome to Github Actions.Hello, FastAPI! 🚀 Your app is live!. I am learning something.tes2"}
+    return {"message": "My Second Website"}
 
 @app.get("/health")  # Optional health check endpoint
 def health_check():
