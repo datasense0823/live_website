6fcdbfbf9b91a77943994d06b6b1d092f06e3311
datasense <datasense.learning@gmail.com>
2025-03-01 09:55:39 -0700

    Deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")  # Ensure the root path ("/") is defined
 def home():
-    return {"message": "My Second Website"}
+    return {"message": "My First Web Deployment through Github Actions"}
 
 @app.get("/health")  # Optional health check endpoint
 def health_check():
