3109abe415fda170e4e7c3456cb82f776172f469
datasense <datasense.learning@gmail.com>
2025-03-16 10:53:42 -0700

    Third

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")  # Ensure the root path ("/") is defined
 def home():
-    return {"message": "I am learning Development 234"}
+    return {"message": "I just made a deployment. "}
 
 @app.get("/health")  # Optional health check endpoint
 def health_check():
